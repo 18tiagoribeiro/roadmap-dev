2ffaac1b0afb25ca0d87c8c8cd7d040740e7d71f
18tiagoribeiro <tiagor270@hotmail.com>
2025-09-25 14:44:16 -0300

    subindo Agenda

---
+++ Etapa3_Csharp_Agenda/Compromisso.cs
@@ -0,0 +1,25 @@
+﻿
+public class Compromisso
+{
+    public string Nome { get; set; }
+    public string Telefone { get; set; }
+    public DateTime Data { get; set; }
+
+    public Compromisso(string nome, string telefone, DateTime data)
+    {
+        Nome = nome;
+        Telefone = telefone;
+        Data = data;
+    }
+
+    public void Mostrar()
+    {
+        Console.WriteLine($"Sr(a) {Nome}, cadastrado com o Telefone: {Telefone}");
+        Console.WriteLine($"Na data escolhida: {Data}");
+        Console.Write($"Muito obrigado!");
+        Console.WriteLine();
+    }
+}
+
+
+
---
+++ Etapa3_Csharp_Agenda/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+List<Compromisso> agenda = new List<Compromisso>();
+
+Console.WriteLine("Digite seu nome: ");
+string nome = Console.ReadLine();
+
+Console.WriteLine("Digite seu telefone: ");
+string telefone = Console.ReadLine();
+
+Console.WriteLine("Digite a data que quer agendar (AAAA-MM-DD): ");
+DateTime data = DateTime.Parse(Console.ReadLine());
+
+if (data >= DateTime.Now)
+{
+    Compromisso novo = new Compromisso(nome, telefone, data);
+    agenda.Add(novo);
+    Console.WriteLine("Compromisso cadastrado com sucesso");
+}
+else
+{
+    Console.WriteLine("Data invalida. Não é possivel agendar em uma data passada.");
+}
+
+foreach (Compromisso item in agenda)
+{
+    Console.WriteLine("\n--- Compromisso ---");
+    item.Mostrar();
+}
